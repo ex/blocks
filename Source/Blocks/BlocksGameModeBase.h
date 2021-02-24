d36ad9827b7eeb5ae715018b8ced7c298fbacb53
ex <exeqtor@gmail.com>
2021-02-24 12:16:37 -0500

    empty Unreal c++ project

---
+++ Source/Blocks/BlocksGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BlocksGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BLOCKS_API ABlocksGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
